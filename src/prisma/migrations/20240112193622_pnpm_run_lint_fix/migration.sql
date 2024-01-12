757054e93c6df6a80440528be51036fcdd0ac3e6
zz1998022 <1074045407@qq.com>
2024-01-13 04:19:35 +0800

    init

---
+++ src/prisma/migrations/20240112193622_pnpm_run_lint_fix/migration.sql
@@ -0,0 +1,2 @@
+-- RenameIndex
+ALTER TABLE `user` RENAME INDEX `User.email_unique` TO `User_email_key`;
---
+++ src/types/index.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DB_HOST: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    // Add more variables as needed
+  }
+}
